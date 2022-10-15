bc96dccad863328b8fa92ec3592b171d64d4e1ff
casperlangat <langatcasper@gmail.com>
2022-10-15 12:14:10 -0700

    Task, program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z' lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+
