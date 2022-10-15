574a73a76e8289b3ef6cab144770da2505068789
casperlangat <langatcasper@gmail.com>
2022-10-15 12:49:46 -0700

    Task, prints alpha in lowercase accept q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char la;
 
-	for (la = 'a'; la = 'z'; la++)
+	for (la = 'a'; la <= 'z'; la++)
 	{
-		if (la != 'e'; la != 'q')
+		if (la != 'e' && la != 'q')
 			putchar(la);
 	}
 
