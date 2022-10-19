a4e05c33aa892f552b5959fbf81d57740821b3d6
casperlangat <langatcasper@gmail.com>
2022-10-19 12:05:55 -0700

    Task, function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,14 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int x;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0')
 
-	_putchar(ld + '0');
-
-	_putchar('\n');
-
-	return (0);
+	return (x);
 }
