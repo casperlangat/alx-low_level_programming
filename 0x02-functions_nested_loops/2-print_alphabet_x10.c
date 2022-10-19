b0b1b7048bddb9aecb4c0bbc1bd66f9a39e21711
casperlangat <langatcasper@gmail.com>
2022-10-19 01:27:21 -0700

    Task, function that prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+		_putchar(ten);
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
+
