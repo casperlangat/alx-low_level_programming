11927c7abbffd24ea0e3564cdb61234849468c60
casperlangat <langatcasper@gmail.com>
2022-10-20 01:32:16 -0700

    Task 1,Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -9,11 +7,11 @@
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
 
-	return (0);
+return (0);
 }
