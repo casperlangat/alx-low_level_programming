8263c9a82ecfc94b62cf350d84e9f20525cb3191
casperlangat <langatcasper@gmail.com>
2022-10-22 05:44:20 -0700

    Task 3, function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,14 @@
  * print_numbers - prints numbers between 0 to 9.
  * Return: no return.
  */
+
 void print_numbers(void)
 {
 int ch;
 
-for (ch = 48; ch < 58; ch++)
+for (ch = 0; ch <= 9; ch++)
 {
-_putchar(ch);
+_putchar((ch % 10) + '0');
 }
 _putchar('\n');
 }
