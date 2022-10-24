61c6c1b0851f8d5007137db50fd2c1553444387f
casperlangat <langatcasper@gmail.com>
2022-10-24 13:34:12 -0700

    Task 4, function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the num except 2 and 4
- * Return: void
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
+
 void print_most_numbers(void)
 {
-	char c;
+int ch;
 
-	for (c = 0; c <= 9; c++)
-	{
-		if (!(c == 2 || c == 4))
-			_putchar(c);
-	}
-	_putchar('\n');
+for (ch = 48; ch < 58; ch++)
+{
+if (ch != 50 && ch != 52)
+_putchar(ch);
+}
+_putchar('\n');
 }
