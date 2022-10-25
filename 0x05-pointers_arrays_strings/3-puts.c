95e34ced6546c1ef513ab77af0b8f885f699f696
casperlangat <langatcasper@gmail.com>
2022-10-25 07:29:29 -0700

    Task 3, function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,16 @@
 
 void _puts(char *str)
 {
-	int count = 0;
+int count = 0;
 
-		while (count >= 0)
-		{
-			if (str[count] == '\0')
-			{
-				_putchar('\n');
-				break;
-			}
-			  _putchar(str[count]);
-			    count++;
+while (count >= 0)
+{
+if (str[count] == '\0')
+{
+_putchar('\n');
+break;
+}
+_putchar(str[count]);
+count++;
 }
 }
