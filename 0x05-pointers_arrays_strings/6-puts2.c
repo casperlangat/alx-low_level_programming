b37c0ef610b9612946b7a1c05e28399e0a48d785
casperlangat <langatcasper@gmail.com>
2022-10-25 13:00:21 -0700

    Task 6, function that prints every other character of a string, starting with the first character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			-putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
