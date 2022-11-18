08a80b18ff03acae9746a4107081c63703a71d6e
casperlangat <langatcasper@gmail.com>
2022-11-18 13:43:35 -0800

    Task 2, function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", current);
 		}
 		else
+		{
 			printf("(nil)");
+		}
 		if (i < n - 1 && separator != NULL)
 		{
 			Printf("%s", separator);
