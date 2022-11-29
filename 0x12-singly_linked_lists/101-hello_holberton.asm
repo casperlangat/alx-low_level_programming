7f2245ccd2174c8f0668ea8a8dd831ab35f664ca
casperlangat <langatcasper@gmail.com>
2022-11-29 12:41:23 -0800

    64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,14 @@
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
+	SECTION .text
+	extent printf
+	global main
+main:	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
